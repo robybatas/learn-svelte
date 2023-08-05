145d014e8b6c90f19cd772d7e7410c350e82caed
roby <roby@sahabatbisnis.co.id>
2023-08-05 08:55:54 +0700

    adjust header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,46 @@
 <div>
-<!--    HEADER  -->
     <div class="w-full h-16 divide-y bg-white border-bottom flex flex-row absolute">
-        <div class="flex-none custom-width-200 border-right grid">
+        <div class="flex-none custom-width border-right grid">
             <div class="place-self-center">
-                logo
+                <img class="h-8" src="https://restock.id/assets/frontend/images/logos/restock-small.png" alt="logo">
             </div>
         </div>
-        <div class="grow pt-5 pl-2">
-            title
+        <div class="grow pt-5 pl-5">
+            <h1 class="font-bold">
+                Pembiayaan
+            </h1>
         </div>
-        <div class="flex-none custom-width-200 pt-5">
-            action
+        <div class="flex-none custom-width pt-3">
+            <div class="relative ml-3">
+                <div class="flex flex-row">
+                    <button type="button" class="relative rounded-full p-1 text-gray-400 ml-2">
+                        <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
+                        </svg>
+                    </button>
+                    <button type="button" class="relative flex rounded-full text-sm ml-3">
+                        <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                    </button>
+                    <div class="ml-2">
+                        <p class="text-sm">
+                            Mas Botak Hmm
+                        </p>
+                        <p class="text-xs font-normal">
+                            Lender
+                        </p>
+                    </div>
+                    <button type="button" class="relative rounded-full p-1 text-gray-400 ml-3 pt-2">
+                        <svg class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
+                            <path d="M160,128a32,32,0,1,1-32-32A32.03164,32.03164,0,0,1,160,128ZM48,96a32,32,0,1,0,32,32A32.03164,32.03164,0,0,0,48,96Zm160,0a32,32,0,1,0,32,32A32.03164,32.03164,0,0,0,208,96Z"/>
+                        </svg>
+                    </button>
+                </div>
+
+            </div>
         </div>
     </div>
     <div class="w-full flex flex-row h-screen">
-        <div class="flex-none custom-width-200 border-right p-6 pt-16">
+        <div class="flex-none custom-width border-right p-6 pt-16">
             <div class="h-4/5 pt-4">
                 menu atas
             </div>
@@ -48,11 +74,8 @@
     .border-right {
         border-right: 2px solid #e9e9e9;
     }
-    .custom-width-200 {
-        width: 200px;
-    }
-    .custom-width-150 {
-        width: 150px;
+    .custom-width {
+        width: 300px;
     }
     .background-content {
         background-color: #f2f5f8;
