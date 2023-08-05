b160ef56cc31c983e5df14a60ffda34caf348b74
roby <roby@sahabatbisnis.co.id>
2023-08-05 08:08:37 +0700

    setup and mockup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,60 @@
+<div>
+<!--    HEADER  -->
+    <div class="w-full h-16 divide-y bg-white border-bottom flex flex-row absolute">
+        <div class="flex-none custom-width-200 border-right grid">
+            <div class="place-self-center">
+                logo
+            </div>
+        </div>
+        <div class="grow pt-5 pl-2">
+            title
+        </div>
+        <div class="flex-none custom-width-200 pt-5">
+            action
+        </div>
+    </div>
+    <div class="w-full flex flex-row h-screen">
+        <div class="flex-none custom-width-200 border-right p-6 pt-16">
+            <div class="h-4/5 pt-4">
+                menu atas
+            </div>
+            <div class="h-28">
+                menu agak bawah
+            </div>
+            <div>
+                menu bawah
+            </div>
+        </div>
+        <div class="grow p-6 pt-16 background-content">
+            <div>
+                <div>
+                    card
+                </div>
+                <div>
+                    card
+                </div>
+            </div>
+            <div>
+                table
+            </div>
+        </div>
+    </div>
+</div>
+
+<style lang="postcss">
+    .border-bottom {
+        border-bottom: 2px solid #e9e9e9;
+    }
+    .border-right {
+        border-right: 2px solid #e9e9e9;
+    }
+    .custom-width-200 {
+        width: 200px;
+    }
+    .custom-width-150 {
+        width: 150px;
+    }
+    .background-content {
+        background-color: #f2f5f8;
+    }
+</style>
